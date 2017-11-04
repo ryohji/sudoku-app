b8c3534383b2814c5c41b0ca4b966e54cbe0bfe9
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2017-11-04 11:19:16 +0900

    Move boxes calculation on created lifecycle hook.

--- js/index.js
+++ js/index.js
@@ -29,32 +29,31 @@ const rootVm = new Vue({
             </div>
         </div>
     </div>`,
-    computed: {
-        boxes: function() {
-            const rows = this.rows;
-            const sliced = rows.map(row => slice(row, 3));
-            return [
-                [].concat(sliced[0][0], sliced[1][0], sliced[2][0]),
-                [].concat(sliced[0][1], sliced[1][1], sliced[2][1]),
-                [].concat(sliced[0][2], sliced[1][2], sliced[2][2]),
-                
-                [].concat(sliced[3][0], sliced[4][0], sliced[5][0]),
-                [].concat(sliced[3][1], sliced[4][1], sliced[5][1]),
-                [].concat(sliced[3][2], sliced[4][2], sliced[5][2]),
-
-                [].concat(sliced[6][0], sliced[7][0], sliced[8][0]),
-                [].concat(sliced[6][1], sliced[7][1], sliced[8][1]),
-                [].concat(sliced[6][2], sliced[7][2], sliced[8][2]),
-            ];
-        },
-    },
     created: function() {
-        this.rows = slice(Array.from(this.board).map(Number), 9);
+        const rows = slice(Array.from(this.board).map(Number), 9);
+        this.rows = rows;
+
+        const sliced = rows.map(row => slice(row, 3));
+        this.boxes = [
+            [].concat(sliced[0][0], sliced[1][0], sliced[2][0]),
+            [].concat(sliced[0][1], sliced[1][1], sliced[2][1]),
+            [].concat(sliced[0][2], sliced[1][2], sliced[2][2]),
+            
+            [].concat(sliced[3][0], sliced[4][0], sliced[5][0]),
+            [].concat(sliced[3][1], sliced[4][1], sliced[5][1]),
+            [].concat(sliced[3][2], sliced[4][2], sliced[5][2]),
+
+            [].concat(sliced[6][0], sliced[7][0], sliced[8][0]),
+            [].concat(sliced[6][1], sliced[7][1], sliced[8][1]),
+            [].concat(sliced[6][2], sliced[7][2], sliced[8][2]),
+        ];
+
         this.memo.data = Array.from({length: 81}).map(_ => [1,2,3,4,5,6,7,8,9,]);
     },
     data: () => new Object({
         board: '060003001200500600007090500000400090800000006010005000002010700004009003700200040', // 朝日新聞beパズル 2017/10/07 掲載分
         rows: undefined,
+        boxes: undefined,
         memo: { hidden: false, },
     }),
 });
