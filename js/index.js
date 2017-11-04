c3ae62cdce17096b746ee7c3fa983f5b4ae428a0
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2017-11-04 10:25:06 +0900

    Hide reduceConcat and make rows visibile as Vue reactive data member.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 const splitAt = (array, n) => [array.slice(0, n), array.slice(n)];
-const reduceConcat = a => a.length ? a.reduce((a, b) => [a].concat(reduceConcat(b))) : [];
-const slice = (array, n) => {
-    var p = array;
-    while (p[1].length !== 0) {
-        p[1] = splitAt(p[1], n);
-        p = p[1];
-    }
-    return reduceConcat(array);
-};
+
+const slice = (() => {
+    const reduceConcat = a => a.length ? a.reduce((a, b) => [a].concat(reduceConcat(b))) : [];
+
+    return (array, n) => {
+        var p = array;
+        while (p[1].length !== 0) {
+            p[1] = splitAt(p[1], n);
+            p = p[1];
+        }
+        return reduceConcat(array);
+    };
+})();
 
 const rootVm = new Vue({
     el: '#app-sudoku',
@@ -48,5 +52,6 @@ const rootVm = new Vue({
     },
     data: () => new Object({
         board: '060003001200500600007090500000400090800000006010005000002010700004009003700200040', // 朝日新聞beパズル 2017/10/07 掲載分
+        rows: undefined,
     }),
 });
