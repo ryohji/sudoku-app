d2596ee35f19eec978b589908a7a8309976f1cb5
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2017-12-04 01:43:57 +0900

    Change memo as Boolean array.

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ const rootVm = new Vue({
     @keydown.55="pointed.value = 7"
     @keydown.56="pointed.value = 8"
     @keydown.57="pointed.value = 9"
-    @keydown.space="pointed && pointed.value && affected.forEach(cell => cell.memo.splice(pointed.value - 1, 1, ''))"
+    @keydown.space="pointed && pointed.value && affected.forEach(cell => cell.memo.splice(pointed.value - 1, 1, false))"
     @mouseenter="event => event.target.focus()"
     >
         <div class="box-row" v-for="span in [[0, 3], [3, 6], [6, 9]]">
@@ -24,12 +24,12 @@ const rootVm = new Vue({
                     :class="{cell: 1, hover: affected.includes(cell), }"
                     @mouseenter="pointed = cell" @mouseleave="pointed = null"
                     >
-                    <span v-if="cell.given" class="given">{{ cell.value }}</span>
-                    <span v-else-if="cell.value">{{ cell.value }}</span>
-                    <div  v-else class="memo" v-for="memo in slice(cell.memo, 3)">
-                        <span v-for="n in memo">{{ n }}</span>
-                    </div>
-                </span>
+                        <span v-if="cell.given" class="given">{{ cell.value }}</span>
+                        <span v-else-if="cell.value">{{ cell.value }}</span>
+                        <div  v-else class="memo" v-for="memo in slice(cell.memo.map((set, i) => set ? i + 1 : ''), 3)">
+                            <span v-for="n in memo">{{ n }}</span>
+                        </div>
+                    </span>
                 </div>
             </div>
         </div>
@@ -55,7 +55,7 @@ const rootVm = new Vue({
         */
         cells: Array.from(
             '060003001200500600007090500000400090800000006010005000002010700004009003700200040' // 朝日新聞beパズル 2017/10/07 掲載分
-        ).map(n => new Object({ given: Number(n), value: Number(n), memo: [1,2,3,4,5,6,7,8,9,], })),
+        ).map(n => new Object({ given: Number(n), value: Number(n), memo: Array(9).fill(true), })),
         pointed: null,
     }),
     methods: {
